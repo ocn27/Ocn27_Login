d2f603a4ea83811f5a95635467b7050b2e024ce2
ocn27 <514671540@qq.com>
2015-07-03 10:48:32 +0800

    第一次上传github

---
+++ js/OcnJs.js
@@ -0,0 +1,39 @@
+//检测平台
+var p = navigator.platform;
+
+var avatar = document.getElementById("avatar");
+var Ocn_Menu = document.getElementById("Ocn_Menu");
+var login = document.getElementById("login");
+var homepage = document.getElementById("homepage");
+
+function icon_Out() {
+	avatar.className = 'avatar avatar_Click';
+
+	Ocn_Menu.style.cssText = "display:block; -webkit-animation: Ocn_Menu_Out 1.5s ease 0s forwards; -moz-animation: Ocn_Menu_Out 1.5s ease 0s forwards; ";
+	login.style.cssText = "display:block; -webkit-animation: login_Out 1.5s ease 0s forwards; -moz-animation: login_Out 1.5s ease 0s forwards; ";
+	homepage.style.cssText = "display:block; -webkit-animation: homepage_Out 1.5s ease 0s forwards; -moz-animation: homepage_Out 1.5s ease 0s forwards; ";
+	avatar.setAttribute("onMouseUp", "icon_In();");
+}
+function icon_In() {
+	avatar.className = 'avatar avatar_Click';
+
+	Ocn_Menu.style.cssText = "display:block; -webkit-animation: Ocn_Menu_In 1.5s ease 0s forwards; -moz-animation: Ocn_Menu_In 1.5s ease 0s forwards; ";
+	login.style.cssText = "display:block; -webkit-animation: login_In 1.5s ease 0s forwards; -moz-animation: login_In 1.5s ease 0s forwards; ";
+	homepage.style.cssText = "display:block; -webkit-animation: homepage_In 1.5s ease 0s forwards; -moz-animation: homepage_In 1.5s ease 0s forwards; ";
+	avatar.setAttribute("onMouseUp", "icon_Out();");
+}
+function get_Reset() {
+	avatar.className = 'avatar';
+}
+
+function homepage_Out() {
+	icon_In();
+	window.location.href="#page2"; 
+}
+
+function go_Back() {
+	window.location.href="#page1"; 
+}
+function login_Out() {
+	window.location.href="http://ocn27.com/wordpress/wp-login.php"; 
+}
---
+++ js/config.js
@@ -0,0 +1,44 @@
+require.config({
+	baseUrl: './js',
+	paths: {
+		'jquery': 'http://cdn.bootcss.com/jquery/1.11.1/jquery.min',
+		'jquery.easings': 'jquery.easings.min',
+		'jquery.slimscroll': 'jquery.slimscroll.min',
+		'jquery.fullPage': 'jquery.fullPage.min',
+		'BaiduMap': 'BaiduMap',
+		'OcnJs': 'OcnJs'
+	},
+	shim: {
+		'jquery.easings': {
+			deps: ['jquery']
+		},
+		'jquery.slimscroll': {
+			deps: ['jquery']
+		},
+		'jquery.fullPage': {
+			deps: ['jquery', 'jquery.easings', 'jquery.slimscroll']
+		}
+	}
+});
+
+require(['jquery.fullPage', 'BaiduMap', 'OcnJs'], function( fullPage, BaiduMap ){
+
+	//FullPage
+	$('#fullpage').fullpage({
+        'css3': true,
+        'sectionsColor': ['#000', '#254875'],
+        anchors: ['page1', 'page2']
+    })
+	$.fn.fullpage.setAllowScrolling(false);
+	$.fn.fullpage.keyboardScrolling(false);
+
+	//BaidMap
+	BaiduMap();
+
+	//Kill IE
+	var isChrome = window.navigator.userAgent.toLowerCase().indexOf('chrome'),
+	    isFireFox = window.navigator.userAgent.toLowerCase().indexOf('firefox');
+	if ( isChrome < 0 && isFireFox < 0 ){
+		$('#kill-ie-bg, #kill-ie').show();
+	}
+});
